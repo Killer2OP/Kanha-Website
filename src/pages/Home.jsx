46bfc5a1da04d42bf4f227138589df2ea71c1ec3
Pratham Mahajan <111378171+Killer2OP@users.noreply.github.com>
2025-03-24 18:26:07 +0530

    Top Things in Kanha Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 
 import Hero from '../container/main/home/Hero'
 import Introduction from '../container/main/home/Introduction'
+import TopThings from '../container/main/home/TopThings'
 import Footer from '../components/Footer'
 
 
 function home() {
-    return (
-        <>
-            <Hero />
-            <Introduction />
-            <Footer/>
-        </>
-    )
+  return (
+  <>
+  <Hero/>
+  <Introduction/>
+  <TopThings/>
+  {/* <Footer/> */}
+  </>
+  )
 }
 
 export default home
