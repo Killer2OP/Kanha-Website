d83e47f52fdb57cf3fb87f46cd455def4262c3f4
divyanshuaaliwal <divyanshuaaliwal@gmail.com>
2025-03-24 16:18:19 +0530

    Header update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { Map, Menu, Search, X, Home, PawPrint, Compass, Hotel, CreditCard } from 'lucide-react';
 import { useMediaQuery } from "react-responsive";
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function Header() {
+    
+    const navItems = [
+        { name: "Home", icon: Home, path: "/" },
+        { name: "Online Safari Booking", icon: PawPrint, path: "#" },
+        { name: "Tour Packages", icon: Compass, path: "#" },
+        { name: "Hotel in Kanha", icon: Hotel, path: "#" },
+        { name: "Pay Now", icon: CreditCard, path: "#" }
+    ];
+
 
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
     const isMobile = useMediaQuery({ maxWidth: 639 });
     const isTablet = useMediaQuery({ minWidth: 640, maxWidth: 1023 });
     const isLaptop = useMediaQuery({ minWidth: 1024 });
 
+    const currentPath = useLocation();
 
-    const navItems = [
-        { name: "Home", icon: Home, path: "/" },
-        { name: "Online Safari Booking", icon: PawPrint },
-        { name: "Tour Packages", icon: Compass },
-        { name: "Hotel in Kanha", icon: Hotel },
-        { name: "Pay Now", icon: CreditCard }
-    ];
 
     const toggleMobileMenu = () => {
         setMobileMenuOpen(!mobileMenuOpen);
@@ -51,7 +53,7 @@ function Header() {
                 <div className="hidden md:flex items-center md:justify-evenly space-x-8">
                     {
                         navItems.map((item, index) => {
-                            return <a key={index} href={item.path || "#"} className="text-white hover:text-gray-200">{item.name}</a>                        
+                            return <a key={index} href={item.path} className="text-white hover:text-gray-200">{item.name}</a>                        
                         })
                     }
                 </div>
@@ -95,19 +97,21 @@ function Header() {
 
                 
                 <div className="px-6 py-8">
-                    {
+                    {   
                         navItems.map((item, index) => (
-                            <a
-                                key={index}
-                                href="#"
-                                className="flex items-center text-white text-lg font-medium py-4 hover:text-amber-400 transition-colors duration-200 group"
-                            >
-                                <item.icon
-                                    size={22}
-                                    className="mr-4 stroke-current group-hover:scale-110 transition-transform duration-200"
-                                />
-                                {item.name}
-                            </a>
+                            currentPath.pathname != item.path && (
+                                <a
+                                    key={index}
+                                    href={item.path}
+                                    className="flex items-center text-white text-lg font-medium py-4 hover:text-amber-400 transition-colors duration-200 group"
+                                >
+                                    <item.icon
+                                        size={22}
+                                        className="mr-4 stroke-current group-hover:scale-110 transition-transform duration-200"
+                                    />
+                                    {item.name}
+                                </a>
+                            )
                         ))
                     }
                 </div>
@@ -118,3 +122,4 @@ function Header() {
 }
 
 export default Header;
+ 
